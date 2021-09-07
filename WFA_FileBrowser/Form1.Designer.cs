013f56c780ff749a9dc171b025bdbbd817b7bcb6
Elvis Segura <elvisseguraa.05@outlook.com>
2021-09-07 18:37:14 -0500

    Agregar archivos de proyecto.

---
+++ WFA_FileBrowser/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace WFA_FileBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboDirPath = new System.Windows.Forms.ComboBox();
+            this.lstCarpetas = new System.Windows.Forms.ListBox();
+            this.lstArchivos = new System.Windows.Forms.ListBox();
+            this.pctImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboDirPath
+            // 
+            this.cboDirPath.FormattingEnabled = true;
+            this.cboDirPath.Location = new System.Drawing.Point(12, 12);
+            this.cboDirPath.Name = "cboDirPath";
+            this.cboDirPath.Size = new System.Drawing.Size(382, 21);
+            this.cboDirPath.TabIndex = 0;
+            this.cboDirPath.SelectedIndexChanged += new System.EventHandler(this.cboDirPath_SelectedIndexChanged);
+            this.cboDirPath.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cboDirPath_KeyPress);
+            // 
+            // lstCarpetas
+            // 
+            this.lstCarpetas.FormattingEnabled = true;
+            this.lstCarpetas.Location = new System.Drawing.Point(15, 48);
+            this.lstCarpetas.Name = "lstCarpetas";
+            this.lstCarpetas.Size = new System.Drawing.Size(172, 264);
+            this.lstCarpetas.TabIndex = 1;
+            this.lstCarpetas.Click += new System.EventHandler(this.lstCarpetas_Click);
+            this.lstCarpetas.SelectedIndexChanged += new System.EventHandler(this.lstCarpetas_SelectedIndexChanged);
+            this.lstCarpetas.DoubleClick += new System.EventHandler(this.lstCarpetas_DoubleClick);
+            // 
+            // lstArchivos
+            // 
+            this.lstArchivos.FormattingEnabled = true;
+            this.lstArchivos.Location = new System.Drawing.Point(205, 47);
+            this.lstArchivos.Name = "lstArchivos";
+            this.lstArchivos.Size = new System.Drawing.Size(188, 264);
+            this.lstArchivos.TabIndex = 2;
+            this.lstArchivos.SelectedIndexChanged += new System.EventHandler(this.lstArchivos_SelectedIndexChanged);
+            this.lstArchivos.DoubleClick += new System.EventHandler(this.lstArchivos_DoubleClick);
+            // 
+            // pctImagen
+            // 
+            this.pctImagen.Location = new System.Drawing.Point(412, 48);
+            this.pctImagen.Name = "pctImagen";
+            this.pctImagen.Size = new System.Drawing.Size(294, 263);
+            this.pctImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pctImagen.TabIndex = 3;
+            this.pctImagen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pctImagen);
+            this.Controls.Add(this.lstArchivos);
+            this.Controls.Add(this.lstCarpetas);
+            this.Controls.Add(this.cboDirPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboDirPath;
+        private System.Windows.Forms.ListBox lstCarpetas;
+        private System.Windows.Forms.ListBox lstArchivos;
+        private System.Windows.Forms.PictureBox pctImagen;
+    }
+}
+
---
+++ WFA_FileBrowser/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_FileBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadComboBoxDefault(); // +
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LoadComboBoxDefault()
+        {
+            //Obtener la lista de unidades de la pc
+            cboDirPath.DataSource = DriveInfo.GetDrives();
+        }
+
+        private void cboDirPath_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                lstCarpetas.DataSource = Directory.GetDirectories(cboDirPath.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void lstArchivos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FileInfo info = new FileInfo(lstArchivos.Text);
+            if (info.Extension == ".jpg" || info.Extension == ".gif" || info.Extension == ".png")
+            {
+                pctImagen.Image = Image.FromFile(lstArchivos.Text);
+            }
+        }
+
+        private void lstArchivos_DoubleClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(lstArchivos.Text);
+
+        }
+
+        private void lstCarpetas_DoubleClick(object sender, EventArgs e)
+        {
+            
+            cboDirPath.Text = lstCarpetas.Text;
+
+            try
+            {
+                lstCarpetas.DataSource = Directory.GetDirectories(cboDirPath.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void lstCarpetas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstCarpetas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                lstArchivos.DataSource = Directory.GetFiles(lstCarpetas.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void cboDirPath_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar == (int)Keys.Enter)
+            {
+                try
+                {
+                    lstCarpetas.DataSource = Directory.GetDirectories(cboDirPath.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
